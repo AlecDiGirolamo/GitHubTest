2dc92b1f44bd766844069e3fa22ce21cea2b671b
Alec DiGirolamo <adigirolamo@carthage.edu>
2020-02-25 10:32:38 -0600

    Square
    Added the square function

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main()
 {
     cout << "GitHubTest: Hello World" << endl;
+    cout << square(2) << endl;
     return 0;
+}
+
+int square(int num)
+{
+    return num * num;
 }
